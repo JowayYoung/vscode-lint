886b0ebe216681582195bd64d1763b0cebb29e20
yangzewei <yangzewei@corp.netease.com>
2020-11-06 17:05:20 +0800

    增加tslint依赖，暂未配置tslint

---
+++ tslint/tslintrc.js
@@ -0,0 +1,99 @@
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		jquery: true,
+		node: true
+	},
+	extends: [
+		"standard-with-typescript",
+		"plugin:@typescript-eslint/recommended"
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2020,
+		// project: "tsconfig.json",
+		sourceType: "module"
+	},
+	plugins: [
+		"@typescript-eslint"
+	],
+	root: true,
+	rules: {
+		"eol-last": [
+			"error",
+			"never"
+		],
+		"handle-callback-err": [
+			"error",
+			"err"
+		],
+		indent: [
+			"error",
+			"tab"
+		],
+		"lines-between-class-members": [
+			"error",
+			"never"
+		],
+		"multiline-ternary": [
+			"off"
+		],
+		"no-async-promise-executor": [
+			"off"
+		],
+		"no-console": [
+			"off"
+		],
+		"no-extend-native": [
+			"off"
+		],
+		"no-new": [
+			"off"
+		],
+		"no-proto": [
+			"off"
+		],
+		"no-return-assign": [
+			"off"
+		],
+		"no-sequences": [
+			"off"
+		],
+		"no-tabs": [
+			"off"
+		],
+		"no-unreachable": [
+			"off"
+		],
+		"no-useless-constructor": [
+			"off"
+		],
+		"object-curly-spacing": [
+			"error",
+			"always"
+		],
+		"operator-linebreak": [
+			"error",
+			"before"
+		],
+		quotes: [
+			"error",
+			"double"
+		],
+		semi: [
+			"error",
+			"always"
+		],
+		"sort-keys": [
+			"error",
+			"asc",
+			{ caseSensitive: false, natural: true }
+		],
+		"space-before-function-paren": [
+			"error",
+			"never"
+		]
+	}
+};
